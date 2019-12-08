d151083f1b732df3026a2a4337163031916210c9
Richard Guerre <guerre.richard@me.com>
2019-12-08 23:21:50 +0100

    added TODOs, socket.io on server and on QueuePage

--- server/server.js
+++ server/server.js
@@ -4,11 +4,19 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const fs = require('fs');
 
-const auth = require('./auth.js');
+{ // Auth config
+  const auth = require('./auth.js');
+  app.use('/', auth);
+}
 
-app.use('/', auth);
+{ // socket io rooms implementation
+  io.on('connection', (socket) => {
+    console.log(`user ${socket.id} has connected`);
+    socket.on('disconnect', () => console.log(`user ${socket.id} has disconnected`))
+  })
+}
 
-app.listen(8888, (err) => {
+http.listen(8888, (err) => { // server listening...
   if(err) throw err;
   console.log('Listening on port 8888...');
 });
--- src/Pages/QueuePage.js
+++ src/Pages/QueuePage.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import io from 'socket.io-client';
 
 import Queue from '../Components/Queue';
 import NowPlaying from '../Components/NowPlaying';
 import AddToQueue from '../Components/AddToQueue';
 
+
 export default function QueuePage() {
+const socket = io('http://localhost:8888/');
 
     return (
         <div className="QueuePage">
