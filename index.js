863f70a2e787811b51b67c2e9d843c51d97f1b89
Richard Guerre <guerre.richard@me.com>
2020-01-04 19:22:06 +0100

    heroku prep5

--- index.js
+++ index.js
@@ -328,7 +328,7 @@ io.on('connection', (socket) => {
 })
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../build'));
+  app.use(express.static('build'));
 
    app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
