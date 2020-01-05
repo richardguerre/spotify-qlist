f96fac76354bb73a3ba066cc5105295f42e7583e
Richard Guerre <guerre.richard@me.com>
2020-01-05 11:13:54 +0100

    heroku prep11

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 // import NavBar from './Components/NavBar';
 import CreatePage from './Pages/CreatePage.js';
